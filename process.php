19044e9652dd3fbafbfa1bbb608c2998d9024f0d
paschalkachi <paschalnnamani10@gmail.com>
2025-07-16 13:06:06 +0100

    Implemented AJAX-based form submission using GET and POST. Connected PHP form to MYSQL database 'ajaxtest' which I created. Was able to add new users to the database by  submission.

--- process.php
+++ process.php
@@ -1,11 +1,23 @@
-<?php
+ <?php 
+
+// Connect to a database
+$conn = mysqli_connect('localhost', 'root', '', 'ajaxtest');
 
 echo 'Processing...';
 
 // Check for POST variable
 if(isset($_POST['name'])){
-    echo 'POST: Your name is '.$_POST['name'];
-}
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    echo 'POST: Your name is '.$name;
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)){
+        echo 'User Added...';
+    } else {
+        echo 'ERROR:'. mysqli_error($conn);
+    }
+} 
 
 // Check for GET variable
 if(isset($_GET['name'])){
